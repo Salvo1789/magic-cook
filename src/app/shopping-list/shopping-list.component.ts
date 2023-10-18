1452e44de522c25cb99b4bd6a24187ad4bf08121
“Salvo1789” <salvo.mercurio89@gmail.com>
2023-10-18 18:51:01 +0200

    Created structural components

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Spoons of sugar', 4),
+    new Ingredient('Onions', 3)
+  ];
+
+  constructor(){
+
+  }
+}
