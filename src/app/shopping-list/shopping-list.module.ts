48958edfcbd739b5362c821aea1e9755597a6be2
“Salvo1789” <salvo.mercurio89@gmail.com>
2024-01-21 21:29:45 +0100

    Created features, shared & core modules

---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+
+import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
+import { ShoppingListComponent } from "./shopping-list.component";
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { AuthGuard } from '../auth/auth.guard';
+import { SharedModule } from '../shared/shared.module';
+import { FormsModule } from '@angular/forms';
+
+
+@NgModule({
+    declarations: [
+        ShoppingListComponent,
+        ShoppingEditComponent
+    ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        RouterModule.forChild([{ path: 'shopping-list', component: ShoppingListComponent, canActivate: [AuthGuard]}]),
+        SharedModule
+    ]
+})
+export class ShoppingListModule{}
